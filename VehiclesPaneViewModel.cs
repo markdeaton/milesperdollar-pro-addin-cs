4345deae6bfdcb32c4f8cf0ae4fc3acea2761b3f
Mark Deaton <medeaton@hotmail.com>
2017-04-25 15:40:58 -0700

    More MVVM Assembly and Namespace name change

--- VehiclesPaneViewModel.cs
+++ VehiclesPaneViewModel.cs
@@ -5,12 +5,55 @@ using System.Text;
 using System.Threading.Tasks;
 using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+using System.Reflection;
+using System.IO;
 
 namespace Esri.APL.MilesPerDollar {
     internal class VehiclesPaneViewModel : DockPane {
-        private const string _dockPaneID = "Esri_APL_MilesPerDollar_VehiclesPane";
+        // Thread locking objects
+        protected object _lockXmlYears = new object();
 
-        protected VehiclesPaneViewModel() { }
+        #region Model variables and properties
+
+        private XDocument _xmlAllVehicles;
+
+        private ObservableCollection<String> _vehicleYears;
+        //private readonly ReadOnlyObservableCollection<String> _readonlyVehicleYears;
+
+        public ObservableCollection<String> VehicleYears {
+            get { return _vehicleYears; }
+            set { SetProperty(ref _vehicleYears, value); }
+        }
+
+        #endregion
+
+        #region CTOR & Initialization
+        protected VehiclesPaneViewModel() {
+            // Set up necessary defaults
+            //_readonlyVehicleYears = new ReadOnlyObservableCollection<String>(_vehicleYears);
+            //BindingOperations.EnableCollectionSynchronization(_readonlyVehicleYears, _lockXmlYears);
+
+        }
+
+        protected override Task InitializeAsync() {
+            // Read vehicles data
+            Uri uri = new Uri("pack://application:,,,/Esri.APL.MilesPerDollar;component/Resources/FE_1984-2018.xml");
+            System.IO.Stream stIn = System.Windows.Application.GetResourceStream(uri).Stream;
+            _xmlAllVehicles = XDocument.Load(stIn);
+
+            var qryYears = from vehicle in _xmlAllVehicles.Root.Elements("vehicle")
+                           orderby vehicle.Attribute("year").Value
+                           select vehicle.Attribute("year").Value;
+            VehicleYears = new ObservableCollection<String>(qryYears.Distinct());
+            return base.InitializeAsync();
+        }
+        #endregion
+
+        #region Dockpane Stuff
+        private const string _dockPaneID = "Esri_APL_MilesPerDollar_VehiclesPane";
 
         /// <summary>
         /// Show the DockPane.
@@ -43,4 +86,6 @@ namespace Esri.APL.MilesPerDollar {
             VehiclesPaneViewModel.Show();
         }
     }
+
+    #endregion
 }
