eac0de57a5dfbc2538666cc7c60f7a59afb3b0c1
Mark Deaton <medeaton@hotmail.com>
2017-04-26 16:01:42 -0700

    Wiring for vehicle selection combo boxes

--- VehiclesPaneViewModel.cs
+++ VehiclesPaneViewModel.cs
@@ -10,6 +10,8 @@ using System.Collections.ObjectModel;
 using System.Windows.Data;
 using System.Reflection;
 using System.IO;
+using System.Windows;
+using System.Globalization;
 
 namespace Esri.APL.MilesPerDollar {
     internal class VehiclesPaneViewModel : DockPane {
@@ -20,12 +22,119 @@ namespace Esri.APL.MilesPerDollar {
 
         private XDocument _xmlAllVehicles;
 
-        private ObservableCollection<String> _vehicleYears;
+        private ObservableCollection<String> _vehicleYears, _vehicleMakes, _vehicleModels, _vehicleTypes;
+        // Since we're updating the dropdowns on the UI thread, no need to use the convoluted
+        // read-only sync pattern shown in some of the samples.
         //private readonly ReadOnlyObservableCollection<String> _readonlyVehicleYears;
+
+        private String _selectedVehicleYear, _selectedVehicleMake, _selectedVehicleModel, _selectedVehicleType;
+        private XElement _selectedVehicle;
 
         public ObservableCollection<String> VehicleYears {
             get { return _vehicleYears; }
             set { SetProperty(ref _vehicleYears, value); }
+        }
+
+        public ObservableCollection<string> VehicleMakes {
+            get { return _vehicleMakes; }
+            set { SetProperty(ref _vehicleMakes, value); }
+        }
+
+        public ObservableCollection<string> VehicleModels {
+            get { return _vehicleModels; }
+            set { SetProperty( ref _vehicleModels, value); }
+        }
+
+        public ObservableCollection<string> VehicleTypes {
+            get { return _vehicleTypes; }
+            set { SetProperty( ref _vehicleTypes, value); }
+        }
+
+        public string SelectedVehicleYear {
+            get { return _selectedVehicleYear; }
+            set {
+                SetProperty( ref _selectedVehicleYear, value);
+                SelectedVehicleMake = SelectedVehicleModel = SelectedVehicleType = null;
+                GetVehicleMakes();
+            }
+        }
+
+        public string SelectedVehicleMake {
+            get { return _selectedVehicleMake; }
+            set {
+                SetProperty(ref _selectedVehicleMake, value);
+                SelectedVehicleModel = SelectedVehicleType = null;
+                GetVehicleModels();
+            }
+        }
+
+        public string SelectedVehicleModel {
+            get { return _selectedVehicleModel; }
+            set {
+                SetProperty(ref _selectedVehicleModel, value);
+                SelectedVehicleType = null;
+                GetVehicleTypes();
+            }
+        }
+
+        public string SelectedVehicleType {
+            get { return _selectedVehicleType; }
+            set {
+                SetProperty(ref _selectedVehicleType, value);
+                IEnumerable<XElement> xVehicles = from vehicle in _xmlAllVehicles.Root.Elements("vehicle")
+                                   where vehicle.Attribute("year").Value == SelectedVehicleYear &&
+                                         vehicle.Attribute("make").Value == SelectedVehicleMake &&
+                                         vehicle.Attribute("model").Value == SelectedVehicleModel &&
+                                         vehicle.Attribute("engine").Value == SelectedVehicleType
+                                   select vehicle;
+                XElement xVehicle = xVehicles.Count() > 0 ? xVehicles.First() : null;
+                SelectedVehicle = xVehicle;
+            }
+        }
+
+        public XElement SelectedVehicle {
+            get { return _selectedVehicle; }
+
+            set {
+                SetProperty(ref _selectedVehicle, value);
+            }
+        }
+
+        private void ReadVehicleData() {
+            // Read vehicles data
+            Uri uri = new Uri("pack://application:,,,/Esri.APL.MilesPerDollar;component/Resources/FE_1984-2018.xml");
+            System.IO.Stream stIn = System.Windows.Application.GetResourceStream(uri).Stream;
+            _xmlAllVehicles = XDocument.Load(stIn);
+        }
+        private void GetVehicleYears() {
+            var qryYears = from vehicle in _xmlAllVehicles.Root.Elements("vehicle")
+                           orderby vehicle.Attribute("year").Value
+                           select vehicle.Attribute("year").Value;
+            VehicleYears = new ObservableCollection<String>(qryYears.Distinct());
+        }
+        private void GetVehicleMakes() {
+            var qryMakes = from vehicle in _xmlAllVehicles.Root.Elements("vehicle")
+                           where vehicle.Attribute("year").Value == SelectedVehicleYear
+                           orderby vehicle.Attribute("make").Value
+                           select vehicle.Attribute("make").Value;
+            VehicleMakes = new ObservableCollection<string>(qryMakes.Distinct());
+        }
+        private void GetVehicleModels() {
+            var qryModels = from vehicle in _xmlAllVehicles.Root.Elements("vehicle")
+                            where vehicle.Attribute("year").Value == SelectedVehicleYear &&
+                                  vehicle.Attribute("make").Value == SelectedVehicleMake
+                            orderby vehicle.Attribute("model").Value
+                            select vehicle.Attribute("model").Value;
+            VehicleModels = new ObservableCollection<string>(qryModels.Distinct());                            
+        }
+        private void GetVehicleTypes() {
+            var qryTypes = from vehicle in _xmlAllVehicles.Root.Elements("vehicle")
+                            where vehicle.Attribute("year").Value == SelectedVehicleYear &&
+                                  vehicle.Attribute("make").Value == SelectedVehicleMake &&
+                                  vehicle.Attribute("model").Value == SelectedVehicleModel
+                            orderby vehicle.Attribute("engine").Value
+                            select vehicle.Attribute("engine").Value;
+            VehicleTypes = new ObservableCollection<string>(qryTypes.Distinct());                            
         }
 
         #endregion
@@ -39,20 +148,15 @@ namespace Esri.APL.MilesPerDollar {
         }
 
         protected override Task InitializeAsync() {
-            // Read vehicles data
-            Uri uri = new Uri("pack://application:,,,/Esri.APL.MilesPerDollar;component/Resources/FE_1984-2018.xml");
-            System.IO.Stream stIn = System.Windows.Application.GetResourceStream(uri).Stream;
-            _xmlAllVehicles = XDocument.Load(stIn);
-
-            var qryYears = from vehicle in _xmlAllVehicles.Root.Elements("vehicle")
-                           orderby vehicle.Attribute("year").Value
-                           select vehicle.Attribute("year").Value;
-            VehicleYears = new ObservableCollection<String>(qryYears.Distinct());
+            ReadVehicleData();
+            GetVehicleYears();
             return base.InitializeAsync();
         }
         #endregion
 
-        #region Dockpane Stuff
+
+        #region Dockpane Plumbing
+
         private const string _dockPaneID = "Esri_APL_MilesPerDollar_VehiclesPane";
 
         /// <summary>
@@ -76,6 +180,7 @@ namespace Esri.APL.MilesPerDollar {
                 SetProperty(ref _heading, value, () => Heading);
             }
         }
+        #endregion
     }
 
     /// <summary>
@@ -87,5 +192,30 @@ namespace Esri.APL.MilesPerDollar {
         }
     }
 
+    #region Value Converters
+
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public class NullToVisibilityConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            System.Diagnostics.Debug.WriteLine("NullToVisibilityConverter");
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+
+    [ValueConversion(typeof(object), typeof(Boolean))]
+    public class NullToIsEnabledConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            System.Diagnostics.Debug.WriteLine("NullToIsEnabledConverter");
+            return !(value == null);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
     #endregion
 }
